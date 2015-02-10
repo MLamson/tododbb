13bef2643817c4d8249e741744d81e730fea7222
Mark Lamson <lamson_mark@yahoo.com>
2015-02-10 13:19:22 -0500

    mocha-server

--- test/spec/test.js
+++ test/spec/test.js
@@ -3,11 +3,17 @@
 (function () {
   'use strict';
 
-  describe('Give it some context', function () {
-    describe('maybe a bit more context here', function () {
-      it('should run here few assertions', function () {
+describe('todo list object', function () {
+	 describe('Creating a new todo list', function () {
 
-      });
-    });
-  });
+	 });
+
+});
+  // describe('Give it some context', function () {
+  //   describe('maybe a bit more context here', function () {
+  //     it('should run here few assertions', function () {
+
+  //     });
+  //   });
+  // });
 })();
